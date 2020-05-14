ae0f1d4abc5bdcc4e12949e6eaa13344817d9dad
cristian <pizarro0823@gmail.com>
2020-05-14 15:01:22 -0500

    app vidrieria

---
+++ Inv/inv.js
@@ -0,0 +1,18 @@
+
+
+    list_inventary = [{
+        cabezal_744: 38000,
+        sillar_744: 45000,
+        jamba_744:39200,
+        traslapes_744:39700,
+        enganche_744: 42000,
+        socalo_inferior_744:37300,
+        socalo_superior_744:27000
+    },
+    ];
+    
+
+
+
+
+
